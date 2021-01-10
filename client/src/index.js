562bc6bc6a7ebce8352f4567a77e9cc056de203f
SethMacP <macpherson.seth@gmail.com>
2021-01-10 10:44:45 -0800

    project initialized, index.js setup with store

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {applyMiddleware, createStore} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import {logger} from 'redux-logger'
+
 import "./index.css";
 import App from "./App";
+import index from './reducers/index'
+
+const store = createStore(index, applyMiddleware(thunk, logger));
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+    <App />
+    </Provider>, 
+
     rootElement
 );
 
